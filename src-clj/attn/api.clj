3f54e8f8d67baf2903e4c29192be9ff57ffc9464
Martin Klepsch <martinklepsch@googlemail.com>
2015-12-05 16:36:51 +0100

    locally working oauth

--- src-clj/attn/api.clj
+++ src-clj/attn/api.clj
@@ -1,12 +1,18 @@
 (ns attn.api
-  (:require [bidi.ring :as bring]
+  (:require [clojure.java.io :as io]
+            [ring.util.response :as r]
+            [ring.middleware.params :as mp]
+            [bidi.ring :as bring]
             [oauth.client :as oauth]))
 
-(def secrets (read-string (slurp "resources/secrets.edn")))
+(def secrets (read-string (slurp (io/resource "secrets.edn"))))
 
-(def oauth_callback_uri "http://attentions.oskarth.com/oauth_callback")
+(defn oauth-callback-uri [dev?]
+  (if dev?
+    "http://127.0.0.1:3000/oauth_callback"
+    "http://attentions.oskarth.com/oauth_callback"))
 
-(defn new-consumer []
+(def consumer
   (oauth/make-consumer (:consumer-key secrets)
                        (:consumer-secret secrets)
                        "https://api.twitter.com/oauth/request_token"
@@ -14,18 +20,43 @@
                        "https://api.twitter.com/oauth/authorize"
                        :hmac-sha1))
 
-(defn new-request-token [consumer]
-  (oauth/request-token consumer oauth_callback_uri))
+;; (defn new-request-token [consumer]
+;;   (oauth/request-token consumer oauth_callback_uri))
 
-(def handler
+;; we need the token and the token secret for /auth as well as for
+;; the callback route because of that we need to save it somewhere
+
+(def tokens (atom {}))
+
+(defn get-token
+  "Get us a new token or return an existing one"
+  ([]
+   (println "Getting a fresh request-token")
+   (let [tkn (oauth/request-token consumer (oauth-callback-uri true))]
+     (swap! tokens assoc (:oauth_token tkn) tkn)
+     (println "Request token retrieved:\n" tkn)
+     tkn))
+  ([oauth-token]
+   (get @tokens oauth-token)))
+
+(defn sign-in-url []
+  (println "Getting request token")
+  (let [tkn (get-token)
+        uri (oauth/user-approval-uri consumer (:oauth_token tkn))]
+    (println "URI generated")
+    uri))
+
+(def app-handler
   (bring/make-handler
-   ["/" {"index.html" (fn [req] {:status 200 :body "ex"})
-         ["articles/" :id "/article.html"] (fn [req] {:status 200 :body "Article"})
-         "oauth_callback" (fn [req] (clojure.pprint/pprint req) (:status 200 :body req))}]))
+   ["/" {"" (fn [_] {:status 200 :body "Hello World!" :headers {"Content-Type" "text/plain"}})
+         "index.html" (fn [req] {:status 200 :body "ex"})
+         ;; ["articles/" :id "/article.html"] (fn [req] {:status 200 :body "Article"})
+         ;; OAuth Flow
+         "auth" (fn [req] (r/redirect (sign-in-url)))
+         "oauth_callback" (fn [req]
+                            (let [verifier (get-in req [:params "oauth_verifier"])
+                                  tkn      (get-in req [:params "oauth_token"])
+                                  access-token (oauth/access-token consumer (get-token tkn) verifier)]
+                              {:status 200 :body req :headers {"Content-Type" "text/plain"}}))}]))
 
-(do
-  (println "So far all good")
-  (def consumer (new-consumer))
-  (println "Made a consumer")
-  (def token (new-request-token consumer))
-  (println "All done"))
+(def handler (-> app-handler mp/wrap-params))
